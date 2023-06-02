bf3127f89287ead745f712e768126ccb5e5a5640
Thiago Rodrigues <thiagothogs@icloud.com>
2023-06-01 22:13:36 -0300

    update code structure schema

--- app/Console/Commands/FetchMarvelHeroes.php
+++ app/Console/Commands/FetchMarvelHeroes.php
@@ -2,48 +2,31 @@
 
 namespace App\Console\Commands;
 
+use App\Services\MarvelApiService;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class FetchMarvelHeroes extends Command
 {
     protected $signature = 'marvel:fetch-heroes';
     protected $description = 'Fetches Marvel heroes and stores them in cache';
 
-    public function handle()
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function handle(): void
     {
         $cacheKey = 'marvelHeroes.all';
+        $marvelApiService = new MarvelApiService();
 
-        $publicKey = env('MARVEL_PUBLIC_KEY');
-        $privateKey = env('MARVEL_PRIVATE_KEY');
-        $ts = time();
-        $stringToHash = $ts . $privateKey . $publicKey;
-        $hash = md5($stringToHash);
-        $basePath = env('MARVEL_BASE_PATH') . '/characters';
-        $offset = 0;
-        $limit = 100;
-        $heroes = [];
+        $heroes = $marvelApiService->getMarvelHeroes();
 
-        do {
-            $params = [
-                'ts' => $ts,
-                'apikey' => $publicKey,
-                'hash' => $hash,
-                'limit' => $limit,
-                'offset' => $offset
-            ];
+        $expiration = Carbon::now()->endOfDay();
 
-            $url = url($basePath) . '?' . http_build_query($params);
-
-            $response = Http::get($url);
-            $responseData = $response->json()['data'] ?? [];
-
-            $heroes = array_merge($heroes, $responseData['results'] ?? []);
-
-            $offset += $limit;
-        } while ($offset < $responseData['total']);
-
-        cache()->put($cacheKey, $heroes, 1440);
+        cache()->put($cacheKey, $heroes, $expiration);
 
         $this->info('Marvel heroes fetched and stored in cache.');
     }
